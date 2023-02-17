0ea6905eee884587edf72ecb63c0225db3ef3921
Chris Bridges <chrisbridges9001@gmail.com>
2023-02-16 22:11:16 -0500

    Fix hashing_speed Benchmark Fix hashing_speed benchmark to reflect hashing function name change 😬

--- benches/hashing_speed.rs
+++ benches/hashing_speed.rs
@@ -1,4 +1,4 @@
-use thu2rcc::calc_c2_hash;
+use thu2rcc::calc_hash;
 use criterion::{
     criterion_group,
     criterion_main,
@@ -9,7 +9,7 @@ fn hashing_speed_benchmark(c: &mut Criterion) {
     let cheat_string = "birdman".to_string();
 
     c.bench_function("hashing speed",
-        |b| b.iter(|| calc_c2_hash(cheat_string.clone()))
+        |b| b.iter(|| calc_hash(cheat_string.clone()))
     );
 }
 
